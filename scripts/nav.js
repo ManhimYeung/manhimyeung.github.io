741f44e77e3d451af12189c6d0a34a1860c3afc1
ManhimYeung <manhim.yeung@gmail.com>
2023-07-23 16:40:29 +0100

    updated theme

---
+++ scripts/nav.js
@@ -0,0 +1,13 @@
+function openNav()
+{
+    document.getElementById("sideNav").style.width = "256px";
+    document.getElementById("nav").style.marginLeft = "256px";
+    document.getElementById("main").style.marginLeft = "256px";
+}
+
+function closeNav()
+{
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("nav").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
